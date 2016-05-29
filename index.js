ca8cdd0eb3a15d3d360bf8643a7d2b7893ebf570
Alex <el.zingo@gmail.com>
2016-05-29 03:07:21 +0100

    Fix indentation in index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 module.exports = function(combine, initialValue, aStream) {
-    return new Promise((resolve, reject) => {
-          let accumulatedValue = initialValue
+  return new Promise((resolve, reject) => {
+    let accumulatedValue = initialValue
 
-          aStream.on('data', (data) => {
-                  accumulatedValue = combine(accumulatedValue, data)
-                })  
+    aStream.on('data', (data) => {
+      accumulatedValue = combine(accumulatedValue, data)
+    })  
 
-        aStream.on('end', () => resolve(accumulatedValue))
-          aStream.on('error', () => reject())
-        })  
+    aStream.on('end', () => resolve(accumulatedValue))
+    aStream.on('error', () => reject())
+  })  
 }
